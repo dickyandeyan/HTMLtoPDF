179748f7b879caf62f70c18ea6bcfc73a2bdfece
Dicky <andeyan@qwords.co.id>
2022-01-10 14:31:30 +0700

    reduce size and many more imporvement

--- assets/js/generatepdf.js
+++ assets/js/generatepdf.js
@@ -1,11 +1,11 @@
 const generatePDF = async () => {
-   const quality = 1
-    html2canvas(document.querySelector('#contentToPrint'), {
-       scale: 0.7
+   var quality = 1
+   html2canvas(document.querySelector('#contentToPrint'), {
+      scale: 0.7
    }).then(
-       (canvas) => {
-           const pdf = new jsPDF('p', 'mm', 'a4')
-           pdf.addImage(canvas.toDataURL('image/png'), 'PNG', 0, 0, 210, 298) //210 to 298
+      (canvas) => {
+          var pdf = new jsPDF('p', 'mm', 'a4', true)
+           pdf.addImage(canvas.toDataURL('image/jpeg'), 'JPEG', 0, 0, 210, 298, 'FAST') //210 to 298
            pdf.save('doc/pdf')
        }
    )
